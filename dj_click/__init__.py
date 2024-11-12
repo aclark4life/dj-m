9e68f7a12cecbc9cc03113d57acf82ea941d51c5
Jeffrey A. Clark <aclark@aclark.net>
2024-11-12 08:45:58 -0500

    Update click commands

--- dj_click/__init__.py
+++ dj_click/__init__.py
@@ -5,24 +5,23 @@ import subprocess
 import sys
 
 
-# PROJECT_FILES = [
-#     "backend",
-#     "manage.py",
-#     "mongo_migrations",
-#     ".eslintrc",
-#     ".stylelintrc.json",
-#     "frontend/",
-#     "home/",
-#     "package.json",
-#     "postcss.config.js",
-# ]
-# 
-# 
-# DEV_DEPENDENCIES = [
-#     "-e git+https://github.com/aclark4life/django@mongodb-5.0.x#egg=django",
-#     "-e git+https://github.com/aclark4life/mongo-python-driver@PYTHON-4575#egg=pymongo",
-#     "-e git+https://github.com/aclark4life/django-mongodb@INTPYTHON-348#egg=django-mongodb",
-# ]
+PROJECT_FILES = [
+    "backend",
+    "manage.py",
+    "mongo_migrations",
+    ".eslintrc",
+    ".stylelintrc.json",
+    "frontend/",
+    "home/",
+    "package.json",
+    "postcss.config.js",
+]
+
+DEV_DEPENDENCIES = [
+    "-e git+https://github.com/aclark4life/django@mongodb-5.0.x#egg=django",
+    "-e git+https://github.com/aclark4life/mongo-python-driver@PYTHON-4575#egg=pymongo",
+    "-e git+https://github.com/aclark4life/django-mongodb@INTPYTHON-348#egg=django-mongodb",
+]
 
 TEST_MODULES = [
     "raw_query",
@@ -39,42 +38,42 @@ def cli():
     pass
 
 
-# @click.command()
-# def clean():
-#     for item in PROJECT_FILES:
-#         try:
-#             if os.path.isfile(item):
-#                 os.remove(item)  # Remove file
-#                 print(f"Removed file: {item}")
-#             elif os.path.isdir(item):
-#                 shutil.rmtree(item)  # Remove directory
-#                 print(f"Removed directory: {item}")
-#             else:
-#                 print(f"Skipping: {item} (not a file or directory)")
-#         except Exception as e:
-#             print(f"Error removing {item}: {e}")
-# 
+@click.command()
+def clean():
+    for item in PROJECT_FILES:
+        try:
+            if os.path.isfile(item):
+                os.remove(item)  # Remove file
+                print(f"Removed file: {item}")
+            elif os.path.isdir(item):
+                shutil.rmtree(item)  # Remove directory
+                print(f"Removed directory: {item}")
+            else:
+                print(f"Skipping: {item} (not a file or directory)")
+        except Exception as e:
+            print(f"Error removing {item}: {e}")
 
-# @click.command()
-# def install():
-#     subprocess.run([sys.executable, "-m", "pip", "install", "-U", "pip"])
-#     with open("requirements.txt", "w") as f:
-#         for dependency in DEV_DEPENDENCIES:
-#             f.write(f"{dependency}\n")
-#     subprocess.run(
-#         [sys.executable, "-m", "pip", "install", "-r", "requirements.txt"],
-#         env={"PIP_SRC": "src"},
-#     )
-#     subprocess.run(
-#         [
-#             sys.executable,
-#             "-m",
-#             "pip",
-#             "install",
-#             "-r",
-#             "src/django/tests/requirements/py3.txt",
-#         ]
-#     )
+
+@click.command()
+def install():
+    subprocess.run([sys.executable, "-m", "pip", "install", "-U", "pip"])
+    with open("requirements.txt", "w") as f:
+        for dependency in DEV_DEPENDENCIES:
+            f.write(f"{dependency}\n")
+    subprocess.run(
+        [sys.executable, "-m", "pip", "install", "-r", "requirements.txt"],
+        env={"PIP_SRC": "src"},
+    )
+    subprocess.run(
+        [
+            sys.executable,
+            "-m",
+            "pip",
+            "install",
+            "-r",
+            "src/django/tests/requirements/py3.txt",
+        ]
+    )
 
 
 @click.command()
@@ -138,8 +137,8 @@ def test(k):
             )
 
 
-# cli.add_command(clean)
-# cli.add_command(install)
+cli.add_command(clean)
+cli.add_command(install)
 cli.add_command(runserver)
 cli.add_command(startproject)
 cli.add_command(test)
